2d254823ccfe275f950b99639af1a07152c0fc41
zhengyi <admin@mczhengyi.top>
2023-12-20 13:24:57 +0000

    :sparkles: Add https://git.mczhengyi.top/zhengyi/luci-app-multi-frpc/issues/6

--- luasrc/controller/frp.lua
+++ luasrc/controller/frp.lua
@@ -1,4 +1,6 @@
 module("luci.controller.frp", package.seeall)
+local uci=require"luci.model.uci".cursor()
+local fs = require "nixio.fs"
 
 function index()
 	if not nixio.fs.access("/etc/config/frp") then
@@ -7,10 +9,13 @@ function index()
 
 	entry({"admin", "services", "frp"}, alias("admin", "services", "frp", "base"), _("Multi Frpc"), 100).dependent = true
 	entry({"admin", "services", "frp", "base"}, cbi("frp/basic"), _("Frp Setting"), 1).leaf = true
-	entry({"admin", "services", "frp", "log"}, cbi("frp/log"), _("Plugin Log"), 2).leaf = true
+	entry({"admin", "services", "frp", "service_log"}, cbi("frp/log"), _("Plugin Log"), 2).leaf = true
+	entry({"admin", "services", "frp", "client_log"}, cbi("frp/client_log"), _("Clent Log"), 3).leaf = true
 	entry({"admin", "services", "frp", "config"}, cbi("frp/config")).leaf = true
 	entry({"admin", "services", "frp", "server"}, cbi("frp/server")).leaf = true
 	entry({"admin", "services", "frp", "status"}, call("act_status")).leaf = true
+	entry({"admin", "services", "frp", "server_list"}, call("get_server")).leaf = true
+	entry({"admin", "services", "frp", "get_log"}, call("get_log")).leaf = true
 end
 
 function act_status()
@@ -19,4 +24,19 @@ function act_status()
 	e.running = luci.sys.call("pidof frpc > /dev/null") == 0
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
+end
+
+function get_server()
+	local ret = {}
+	uci.foreach("frp", "server", function (s) 
+		ret[#ret + 1] = s["name"]
+	end)
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(ret) 
+end
+
+function get_log() 
+	local name = luci.http.formvalue("name")
+	local log = fs.readfile(string.format("/var/etc/frp/frpc-%s.log", name))or"NOT FOUND"
+	luci.http.write(log)
 end
